a4d0ece3e125ae2c819f0739c14f23338dec41d9
Logan Campbell <lcampbellmcpherson@zendesk.com>
2021-12-16 16:16:34 +1100

    golfed and added qr gen

--- long.rb
+++ long.rb
@@ -1,11 +1,4 @@
-#!/usr/bin/env ruby -r json -r yaml -r open-uri
-p'[t]ickets/[u]sers?'
-t=gets.chomp
-p'field?'
-f=gets.chomp
-p'value?'
-v=JSON.parse(gets)
-p t
-u="http://shorturl.at/#{{t:'lxTY3',u:'nHIS8'}[t.to_sym]}"
-p u
-puts JSON.parse(open(u).read).select{|r|[f]==v}.to_yaml
+#!/usr/bin/env ruby -ryaml -rjson -ropen-uri
+t,f,v=ARGV
+u='https://lcampbell-zendesk.github.io/qrgolf/'+t
+puts JSON.load(open(u)).filter{|r|r[f]==JSON.load(v)}.to_yaml
